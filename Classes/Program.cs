c89695b072072e7e6487b58b61e49a02e4a2f34e
michaelmiles1 <michael.gage.miles@gmail.com>
2019-09-30 18:01:36 -0700

    Initial commit

---
+++ Classes/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using static System.Console;
+
+namespace Classes
+{
+    class Staff
+    {
+        private string nameOfStaff;
+        private const int hourlyRate = 30;
+        private int hWorked;
+
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (value > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+        }
+
+        public void PrintMessage()
+        {
+            WriteLine("Calculating Pay...");
+        }
+
+        public int CalculatePay()
+        {
+            PrintMessage();
+
+            int staffPay = hWorked * hourlyRate;
+
+            if (hWorked > 0)
+                return staffPay;
+            else
+                return 0;
+        }
+
+        public int CalculatePay(int bonus, int allowance)
+        {
+            PrintMessage();
+
+            if (hWorked > 0)
+                return hWorked * hourlyRate + bonus + allowance;
+            else
+                return 0;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
